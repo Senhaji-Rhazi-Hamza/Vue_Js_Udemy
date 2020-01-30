15dc7d13c5dc0b209d2a8eab97843d1cf521e86e
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2020-01-30 17:24:14 +0100

    seen how to initialize vue-cli project

---
+++ udemy_solutions/section_6/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+//var ipdb = require('index')
+//var bst = new ipdb.BaseStation('c:/tiantexin/download/base_station.ipdb');
+//console.log("Hello World")
+//console.log(App)
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
