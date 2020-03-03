0f6f46ad4214d87b34aea5d8d08a6411ee8c3675
SENHAJIRHAZI <hamza.senhaji-rhazi@artefact.com>
2020-03-03 17:10:13 +0100

    add section 11 exercice solution

---
+++ udemy_solutions/section_11/form_control_exercice/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <form v-if="!submitted">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- Exercise 1 -->
+          <!-- Create a Signup Form where you retrieve the following Information -->
+          <!-- Full Name (First Name + Last Name) -->
+          <!-- Mail -->
+          <!-- Password -->
+          <!-- Store Data? Yes/No -->
+
+          <!-- Exercise 2 -->
+          <!-- Only display the Form if it has NOT been submitted -->
+          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+          <!-- Exercise 3 -->
+          <!-- Edit the Example from above and create a custom "Full Name" Control -->
+          <!-- which still holds the First Name and Last Name Input Field -->
+          <div class="form-group">
+            <label for="firstName">First Name</label>
+            <input type="text" id="firstName" class="form-control" v-model="firstName" />
+          </div>
+          <div class="form-group">
+            <label for="lastName">Last Name</label>
+            <input type="text" id="lastName" class="form-control" v-model="lastName" />
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" id="email" class="form-control" v-model="email" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <div class="form-group">
+            <label for="storedDB">
+              <input type="checkbox" id="storedDB" value="storedDB" v-model="storedDB" /> Stored in Database
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <button class="btn btn-primary" @click="onSubmit">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{firstName}} {{lastName.toUpperCase()}}</p>
+            <p>Mail: {{email}}</p>
+            <p>Password:</p>
+            <p>
+              Store in Database?:
+              <span v-if="storedDB">Yes</span>
+              <span v-else>No</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      storedDB: false,
+      submitted: false
+    }
+  },
+  methods: {
+      onSubmit : function() {
+      this.submitted = true
+      }
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ udemy_solutions/section_11/form_control_exercice/src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <input type="text" id="firstName">
+      <hr>
+       <input type="text" id="lastName">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
